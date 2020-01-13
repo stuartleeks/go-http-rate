b1aca825ba0401a643b08abbb39fbd94300d2cb5
Stuart Leeks <stuartle@microsoft.com>
2020-01-13 12:54:24 +0000

    Add limiting

--- server/main.go
+++ server/main.go
@@ -6,6 +6,8 @@ import (
 	"time"
 
 	"github.com/gorilla/mux"
+
+	"golang.org/x/time/rate"
 )
 
 func main() {
@@ -13,13 +15,29 @@ func main() {
 	router := mux.NewRouter().StrictSlash(true)
 
 	router.HandleFunc("/", index)
+	router.HandleFunc("/", index)
 	router.NotFoundHandler = http.HandlerFunc(notFoundPage)
 	router.MethodNotAllowedHandler = http.HandlerFunc(methodNotAllowed)
 
 	port := ":8080"
 	print("API running on http://localhost" + port + "\n")
 
-	log.Fatal(http.ListenAndServe(port, router))
+	log.Fatal(http.ListenAndServe(port, limit(router)))
+}
+
+var limiter = rate.NewLimiter(10, 3)
+
+func limit(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if limiter.Allow() {
+			next.ServeHTTP(w, r)
+		} else {
+			log.Print("429 response")
+			t := time.Now().UTC()
+			responseText := t.Format("2006-01-02T15:04:05") + "-429 Busy\n"
+			http.Error(w, responseText, http.StatusTooManyRequests)
+		}
+	})
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
